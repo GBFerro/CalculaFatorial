0c0d54bfbf58b33095f9e93059b5f363397dba54
GBFerro <giovani.ferro@usp.br>
2023-03-05 18:33:49 -0300

    Add project files.

---
+++ Fatorial/Program.cs
@@ -0,0 +1,13 @@
+﻿int num, res;
+
+Console.WriteLine("Digite o número: ");
+num = int.Parse(Console.ReadLine());
+
+res = 1;
+
+for (int i = num; i > 0; i--)
+{
+    res *= i;
+}
+
+Console.WriteLine(res);
