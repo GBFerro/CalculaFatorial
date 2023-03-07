efbb748c348c54bf7f8d1a33625c297f3a13b706
GBFerro <giovani.ferro@usp.br>
2023-03-06 23:40:50 -0300

    tratamento de erro e funcao recursiva para calculos

--- Fatorial/Program.cs
+++ Fatorial/Program.cs
@@ -1,13 +1,46 @@
-﻿int num, res;
+﻿int valor, res;
 
-Console.WriteLine("Digite o número: ");
-num = int.Parse(Console.ReadLine());
-
-res = 1;
-
-for (int i = num; i > 0; i--)
+string Imenu()
 {
-    res *= i;
+    Console.WriteLine("Digite um valor: ");
+    return Console.ReadLine();
 }
 
-Console.WriteLine(res);
+valor = retornaErro();
+res = fatorial(valor);
+
+Console.WriteLine($"O fatorial de {valor} é: {res}");
+
+int retornaErro()
+{
+    string stringValor = Imenu();
+    int num;
+    if (!int.TryParse(stringValor, out num))
+    {
+        Console.WriteLine("Informe um número válido");
+        return retornaErro();
+    }
+    else
+    {
+        if ((num > 12)||(num < 0))
+        {
+            Console.WriteLine("O valor extrapola o limite");
+            return retornaErro();
+        }
+
+    }
+    return num;
+}
+
+
+int fatorial(int valor) 
+{
+    if (valor == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (valor * fatorial(valor - 1));
+    }
+}
